8fced4e335764b37f455d07bee33d1d0e0dc9f76
Kelsy Vaughn <angelstate@gmail.com>
2020-03-18 09:35:39 -0700

    rebuild components file

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,40 +1,17 @@
-/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable camelcase */
 import React from 'react';
-import {white} from 'color-name';
-// import PropTypes from 'prop-types';
-// import './photoStyle.css';
 
 const Photo = ({ photo }) => {
-  const styles = {
-    image: {
-      width: '75%',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '50%',
-    },
-    title: {
-      fontSize: 20,
-      color: white,
-    },
-  };
-
-  const {
-    // eslint-disable-next-line react/prop-types
-    index, user_name, photo_title, date_added, url_address, comment,
-  } = photo;
+  const { id, user_name, photo_title, date_added, url_address, comment } = photo;
   return (
-    <div id={`card-${index}`} className="card">
-      <img className="image" style={styles.image} src={url_address} />
-      <div className="details">
-        <span className="index">{index}</span>
-        <p className="title" style={styles.title}>{photo_title}</p>
-        <ul>
-          <li className="date">{date_added}</li>
-          <li className="user-name">{user_name}</li>
-          <li className="comment">{comment}</li>
-        </ul>
+    <div>
+      <div className="flex-container">
+        <img className="image" alt="" src={url_address} />
       </div>
+      <div className="title">{photo_title}</div>
+      <div className="subtitle">{date_added.slice(0, 10)}</div>
+      <strong className="title">{user_name}</strong>
+      <div className="subtitle">{comment}</div>
     </div>
   );
 };
--- client/src/components/PhotoCarousel.jsx
+++ client/src/components/PhotoCarousel.jsx
@@ -4,15 +4,12 @@ import PhotoCard from './PhotoCard.jsx';
 // import { View, ScrollView } from 'react-native';
 
 class PhotoCarousel extends Component {
-  // scrollRef = React.createRef();
   constructor(props) {
     super(props);
     this.state = {
       photos: [],
-      photoIndex: 0,
 
     };
-    this.nextPhoto = this.nextPhoto.bind(this);
   }
 
   componentDidMount() {
@@ -24,29 +21,11 @@ class PhotoCarousel extends Component {
     });
   }
 
-  nextPhoto() {
-  //   const newIndex = this.state.photos[+1];
-  //   this.setState({
-  //     photos,
-  //   });
-  }
-
-  // previousPhoto = () => {
-
-  // }
-
   render() {
     const { photos } = this.state;
-    // const { photoIndex } = this.state;
     return (
       <div className="carousel">
-        Hello Photos!
-        <PhotoCard photos={photos} />
-        <button type="button" onClick={() => this.nextPhoto()}>Next</button>
-        <button type="button">Previous</button>
-        <form action="/profile" method="post" encType="multipart/form-data">
-          <input type="file" name="avatar" />
-        </form>
+          <PhotoCard photos={photos} />
       </div>
     );
   }
