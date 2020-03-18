bd4fc7e8965850f9a631d15e0bc277850eab9c11
Kelsy Vaughn <angelstate@gmail.com>
2020-03-18 09:46:23 -0700

    remove extra files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
+  verbose: true,
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
