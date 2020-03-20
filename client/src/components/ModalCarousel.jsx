99bba4bda41f111766cf3ed964a7db92df513073
Kelsy Vaughn <angelstate@gmail.com>
2020-03-20 15:09:16 -0700

    complete single-photo render of modal and click through functions

--- client/src/components/ModalCarousel.jsx
+++ client/src/components/ModalCarousel.jsx
@@ -5,22 +5,60 @@ class ModalCarousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainPhoto: 0,
+      selectedPhoto: 0,
     };
+    this.prevPhoto = this.prevPhoto.bind(this);
+    this.nextPhoto = this.nextPhoto.bind(this);
+  }
+
+  prevPhoto() {
+    const { photos } = this.props;
+    if (this.state.selectedPhoto <= 1) {
+      this.setState({
+        selectedPhoto: 0,
+      });
+    }
+    this.setState({
+      selectedPhoto: this.state.selectedPhoto - 1 || 0,
+    });
+  }
+
+  nextPhoto() {
+    const { photos } = this.props;
+    if (this.state.selectedPhoto <= photos.length - 1) {
+      this.setState({
+        selectedPhoto: photos.length - 1,
+      })
+    }
+    this.setState({
+      selectedPhoto: this.state.selectedPhoto + 1,
+    });
   }
 
   render() {
-    const { mainPhoto } = this.state;
-    const { photos, show } = this.props;
+    const { selectedPhoto } = this.state;
+    const { photos, show, handleClose } = this.props;
     const showHideClassName = show ? 'model display-block' : 'model display-none';
     return (
       <div>
         <div className={showHideClassName}>
           <section className="modal-main">
+            <div className="close-button"> 
+            <button onClick={handleClose}>close X</button>
+            </div>
             <ModalCard
-              photos={photos}/>
-            <button onClick={this.hideModel}>close X</button>
+              photos={photos} selectedPhotoIndex={selectedPhoto} />
           </section>
+          <div className="arrows">
+          <div
+            type="click"
+            className="modal-slide-arrow-left"
+            onClick={this.prevPhoto}/>
+          <div
+            type="click"
+            className="modal-slide-arrow-right"
+            onClick={this.nextPhoto}/>
+        </div>
         </div>
       </div>
     );
