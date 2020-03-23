23652b1383ff65930762c767eda650d59b237833
Kelsy Vaughn <angelstate@gmail.com>
2020-03-23 09:13:16 -0700

    little formatting touch-ups

--- client/src/components/ModalSideBar.jsx
+++ client/src/components/ModalSideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SideBar from './SideBar.jsx';
 
 const ModalSideBar = ({ photos }) => {
-  return(
+  return (
     <div className="modal-side-bar">
       {photos.map((photo) => (
         <SideBar key={photo._id} photo={photo} />
