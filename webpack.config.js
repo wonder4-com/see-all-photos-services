2bb641a34fbaae7f51e385a58b5d8c18c02ace4d
Kelsy Vaughn <angelstate@gmail.com>
2020-03-24 19:12:57 -0700

    config change

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,19 @@ module.exports = {
   entry: path.join(__dirname, '/client/src/index.jsx'),
   module: {
     rules: [
-      {
-        test: /\.(png|jp(e*)g|svg|gif|jsx)$/,
+        {
+          test: /\.css$/,
+          loader: 'style-loader',
+        },
+        {
+          test: /\.css$/,
+          loader: 'css-loader',
+          query: {
+            modules: true,
+          },
+        },
+          {
+        test: [/\.jsx$/],
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -23,26 +34,15 @@ module.exports = {
 };
 
 
-// module.exports = {
-//   entry: './client/src/index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'client/dist'), //this needed to be created 
+// {
+//   test: [/\.jsx$/],
+//   exclude: /node_modules/,
+//   use: {
+//     loader: 'babel-loader',
+//     options: {
+//       presets: ['@babel/preset-react', '@babel/preset-env'],
+//     },
 //   },
-//   module: {
-//     rules: [
-//       {
-//         test: /.js$|.jsx$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-react', '@babel/preset-env']
-//           }
-//         },
-//       },
-//     ],
-//   },
-// };
+// },
 
-// name: 'images/[hash]-[name].[ext]',
+// /\.(png|jp(e*)g|svg|gif|jsx)$/
